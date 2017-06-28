c9f345436ddd8442598a12df3d459bf28de561a0
renyu <ry930071257@outlook.com>
2017-06-28 18:18:57 +0800

    vue-demo

---
+++ work/todos/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <first></first>
+  </div>
+</template>
+
+<script>
+import First from './components/first.vue'
+
+export default {
+  name: 'app',
+  components: {First}
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ work/webpack_demo/src/components/first.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="first">
+    <h1>我是第一个组件</h1>
+    <p>我想输出数据：<span class="msg">{{msg}}</span></p>
+  </div>
+</template>
+
+<script>
+    export default{
+      name: 'first',
+      data () {
+        return {
+          msg: '我说动态数据'
+        }
+      }
+    }
+</script>
+
+<style>
+  .msg {
+    color : yellow;
+  }
+</style>
